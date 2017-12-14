97e317fcb42f50ca596ab4f4c2eb3e617a5433ea
mazjin <mazjin55555@gmail.com>
2017-12-14 13:09:33 +0000

    adjust interrogator page spacing so there is more space available for table display

--- analysis/templates/analysis/interrogateScript.js
+++ analysis/templates/analysis/interrogateScript.js
@@ -6,4 +6,7 @@ $(document).ready(function(){
 	$("#id_datadrop_select").on("change",function(){
 		$("[for=id_match_datadrop_by_name]").show();
 		};
+	// $("#contentdiv").addClass("container-fluid");
+	$("#containerdiv").removeClass("container").addClass("container-fluid");
+
 	};)
